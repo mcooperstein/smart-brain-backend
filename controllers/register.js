864712ec91d8099a5a731ae2690423ea692c7793
Marc Cooperstein <mcooperstein@gmail.com>
2018-06-02 13:05:49 -0700

    configuing api call to backend 6

--- controllers/register.js
+++ controllers/register.js
@@ -30,18 +30,26 @@ const handleRegister = (req,res,db,bcrypt) => {
 }
 
 const getProfileRank = (req,res,db,bcrypt) => {
-  const {email,name,password} = req.body;
-  if(!email || !name || !password) {
-    return res.status(400).json('unable to register');
+  const {email, password, name} = req.body;
+  if(!email || !password || !name) {
+    return res.status(400).json('incorrect form submission');
   }
-  db.select('*').from('users')
-  .then(users => {
-    if(users.length) {
-      res.json(users)
-    } else {
-      res.status(400).json('users not found')
-    }
-  }).catch(err => res.status(400).json('error getting users ranks'))
+
+  db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0].hash)
+      if (isValid) {
+        return db.select('*').from('users')
+          .then(users => {
+            res.json(users)
+          })
+          .catch(err => res.status(400).json('fail 1'))
+      } else {
+        res.status(400).json('incorrect username/password combination')
+      }
+    })
+    .catch(err => res.status(400).json('fail 2'))
 }
 
 module.exports = {
